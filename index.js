71aa1855f4f28f800e3dc7bd2c5bb9840ba9aab1
kieyn.parks <kieyn.parks@snhu.edu>
2021-09-18 05:35:23 -0400

    bubble pop and reset functionality

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var d = document.querySelectorAll('.animation-area li');
+var c = document.querySelectorAll('#chn1').classList;
+
+var timer;
 var audio1 = new Audio("audio1.wav");
 var audio2 = new Audio("audio2.wav");
 var audio3 = new Audio("audio3.wav");
@@ -6,26 +9,51 @@ var audio4 = new Audio("audio4.wav");
 var audio5 = new Audio("audio5.wav");
 var audio6 = new Audio("audio6.wav");
 
+function reset(index){
+timer =   setTimeout(function(){
+  d[index].style.display = "flex";
+}, 1000);}
+
+
+// console.log(bubble);
+
 // console.log(d[0].innerHTML);
-console.log(d[0].id);
+// console.log(d);
+
 
 d[0].onclick = function(){
   audio1.play();
+  d[0].style.display = "none";
+  // d[0].style.display = "flex";
+  reset(0);
 };
 
 d[1].onclick = function(){
   audio2.play();
+  console.log(this.textContent);
+  // d[0].style.visibility = "initial";
+  d[1].style.display = "none";
+  reset(1);
+
 };
 d[2].onclick = function(){
   audio3.play();
+  d[2].style.display = "none";
+  reset(2);
 };
 d[3].onclick = function(){
   audio4.play();
+  d[3].style.display = "none";
+  reset(3);
 };
 
 d[4].onclick = function(){
   audio5.play();
+  d[4].style.display = "none";
+  reset(4);
 };
 d[5].onclick = function(){
   audio6.play();
+  d[5].style.display = "none";
+  reset(5);
 };
